09fe1f8596af90cfb7b92ee89c454080d2a30aa7
laster18 <34766880+laster18@users.noreply.github.com>
2019-03-10 02:13:41 +0900

    fix indent

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,53 +3,53 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
-	mode: 'development',
-	entry: './src/index.tsx',
-	output: {
-		filename: 'static/js/bundle.js',
-		path: path.resolve(__dirname, 'dist')
-	},
-	devtool: 'inline-source-map',
-	devServer: {
-		contentBase: path.join(__dirname, '/dist'),
-		historyApiFallback: true,
-		port: 3000
-	},
-	module: {
-		rules: [
-			{
-				test: /\.*(ts|tsx)$/,
-				exclude: /node_modules/,
-				use: [
-					{
-						loader: 'thread-loader',
-						options: {
-							workers: require('os').cpus().length - 1,
-							poolTimeout: Infinity
-						}
-					},
-					{
-						loader: 'ts-loader',
-						options: {
-							configFile: 'tsconfig.dev.json',
-							happyPackMode: true
-						}
-					}
-				]
-			}
-		]
-	},
-	resolve: {
-		extensions: ['.ts', '.tsx', '.js', '.jsx']
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: 'src/index.html',
-			filename: 'index.html'
-		}),
-		new ForkTsCheckerWebpackPlugin({
-			tsconfig: 'tsconfig.dev.json',
-			checkSyntacticErrors: true
-		})
-	]
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    filename: 'static/js/bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, '/dist'),
+    historyApiFallback: true,
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.*(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'thread-loader',
+            options: {
+              workers: require('os').cpus().length - 1,
+              poolTimeout: Infinity
+            }
+          },
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: 'tsconfig.dev.json',
+              happyPackMode: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html'
+    }),
+    new ForkTsCheckerWebpackPlugin({
+      tsconfig: 'tsconfig.dev.json',
+      checkSyntacticErrors: true
+    })
+  ]
 };
