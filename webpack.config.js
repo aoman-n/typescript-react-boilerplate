97906d59a7d068280c7d8a081b8de7cba16c1fea
aoba <ipbdipbd00@gmail.com>
2019-03-09 22:30:02 +0900

    Reduce the build time

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,35 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
+	output: {
+		filename: 'bundle.js',
+		publicPath: '/'
+	},
 	module: {
 		rules: [
 			{
 				test: /\.*(ts|tsx)$/,
 				exclude: /node_modules/,
-				use: [{ loader: 'ts-loader' }]
+				use: [
+					{
+						loader: 'thread-loader',
+						options: {
+							workers: require('os').cpus().length - 1,
+							poolTimeout: Infinity
+						}
+					},
+					{
+						loader: 'babel-loader'
+					},
+					{
+						loader: 'ts-loader',
+						options: {
+							happyPackMode: true
+						}
+					}
+				]
 			}
 		]
 	},
@@ -17,6 +40,7 @@ module.exports = {
 		new HtmlWebpackPlugin({
 			template: 'src/index.html',
 			filename: 'index.html'
-		})
+		}),
+		new ForkTsCheckerWebpackPlugin({ checkSyntacticErrors: true })
 	]
 };
