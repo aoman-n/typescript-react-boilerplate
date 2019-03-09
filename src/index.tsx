9162ca8238c9db0b39ed7501066ff45ed31bd9c1
laster18 <34766880+laster18@users.noreply.github.com>
2019-03-10 02:15:21 +0900

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -2,22 +2,22 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 interface HelloProps {
-	name: string;
+  name: string;
 }
 
 const Hello: React.FC<HelloProps> = ({ name }) => <h1>Hello, {name}!!</h1>;
 
 const App: React.FC = () => {
-	const [count, setCount] = React.useState(0);
-	return (
-		<React.Fragment>
-			<Hello name="React & Typescript" />
-			<h3>
-				Count: {count}
-				<button onClick={() => setCount(count + 1)}>count up</button>
-			</h3>
-		</React.Fragment>
-	);
+const [count, setCount] = React.useState(0);
+  return (
+    <React.Fragment>
+      <Hello name="React & Typescript" />
+      <h3>
+        Count: {count}
+        <button onClick={() => setCount(count + 1)}>count up</button>
+      </h3>
+    </React.Fragment>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
