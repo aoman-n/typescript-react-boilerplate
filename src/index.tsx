863b8e3d107b7e3bac9142c1d5198ab24fa4676e
aoba <ipbdipbd00@gmail.com>
2019-03-10 15:25:57 +0900

    Add styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import styled from 'styled-components';
 import menu from './menu.png';
 
 interface HelloProps {
@@ -15,11 +16,23 @@ const App: React.FC = () => {
       <Hello name="React & Typescript" />
       <h3>
         Count: {count}
-        <button onClick={() => setCount(count + 1)}>count up</button>
+        <Button onClick={() => setCount(count + 1)}>count up</Button>
       </h3>
       <img src={menu} />
     </React.Fragment>
   );
 };
+
+const Button = styled.button`
+  font-size: 1.1rem;
+  color: skyblue;
+  border: solid 1px skyblue;
+  font-weight: bold;
+  padding: 10px 15px;
+  &:hover {
+    color: white;
+    background: skyblue;
+  }
+`;
 
 ReactDOM.render(<App />, document.getElementById('root'));
