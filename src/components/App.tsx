a286091ce0022d55eb1ef4f188aa84132334aec9
aoba <ipbdipbd00@gmail.com>
2019-03-12 15:53:05 +0900

    Create todo list using redux

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,38 +1,55 @@
+// import * as React from 'react';
+// import styled from 'styled-components';
+
+// interface HelloProps {
+//   name: string;
+// }
+
+// const Hello: React.FC<HelloProps> = ({ name }) => <h1>Hello, {name}!!</h1>;
+
+// const App: React.FC = () => {
+//   const [count, setCount] = React.useState(0);
+//   return (
+//     <>
+//       <Hello name="React & Typescript" />
+//       <h3>
+//         Count: {count}
+//         <Button onClick={() => setCount(count + 1)}>count up</Button>
+//       </h3>
+//     </>
+//   );
+// };
+
+// const Button = styled.button`
+//   font-size: 1.1rem;
+//   color: skyblue;
+//   border: solid 1px skyblue;
+//   font-weight: bold;
+//   padding: 10px 15px;
+//   cursor: pointer;
+//   outline: none;
+//   &:hover {
+//     color: white;
+//     background: skyblue;
+//     transition: 0.4s;
+//   }
+// `;
+
+// export default App;
+
 import * as React from 'react';
-import styled from 'styled-components';
+import AddTodo from '../containers/AddTodo';
+import VisibleTodoList from '../containers/VisibleTodoList';
+import Footer from './Footer';
 
-interface HelloProps {
-  name: string;
-}
-
-const Hello: React.FC<HelloProps> = ({ name }) => <h1>Hello, {name}!!</h1>;
-
-const App: React.FC = () => {
-  const [count, setCount] = React.useState(0);
+const component: React.SFC = () => {
   return (
-    <>
-      <Hello name="React & Typescript" />
-      <h3>
-        Count: {count}
-        <Button onClick={() => setCount(count + 1)}>count up</Button>
-      </h3>
-    </>
+    <div>
+      <AddTodo />
+      <VisibleTodoList />
+      <Footer />
+    </div>
   );
 };
 
-const Button = styled.button`
-  font-size: 1.1rem;
-  color: skyblue;
-  border: solid 1px skyblue;
-  font-weight: bold;
-  padding: 10px 15px;
-  cursor: pointer;
-  outline: none;
-  &:hover {
-    color: white;
-    background: skyblue;
-    transition: 0.4s;
-  }
-`;
-
-export default App;
+export default component;
