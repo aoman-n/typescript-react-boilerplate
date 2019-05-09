ea9e020120de73c7e3042db4219ee660c16fb443
aoba <ipbdipbd00@gmail.com>
2019-05-09 21:59:14 +0900

    Install redux-saga react-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 import { rootReducer } from './modules';
@@ -10,7 +11,9 @@ const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
 );
